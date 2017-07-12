762ec390a970b3faf22f53a11ee6cb3202bfad08
Nikita Zhelonkin <nikita@zengalt.ru>
2017-07-12 15:29:47 +0300

    Add image button compat

--- lib/src/main/java/ru/nikitazhelonkin/buttoncompat/ImageButtonCompat.java
+++ lib/src/main/java/ru/nikitazhelonkin/buttoncompat/ImageButtonCompat.java
@@ -1,8 +1,90 @@
 package ru.nikitazhelonkin.buttoncompat;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatImageButton;
+import android.util.AttributeSet;
+
 /**
  * Created by nikita on 12.07.17.
  */
 
-public class ImageButtonCompat {
+public class ImageButtonCompat extends AppCompatImageButton {
+
+    private ButtonCompatImpl mImpl;
+
+    public ImageButtonCompat(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public ImageButtonCompat(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public ImageButtonCompat(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs, defStyleAttr);
+    }
+
+    private void init(AttributeSet attrs, int defStyleAttr) {
+        getImpl().init(attrs, defStyleAttr);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        getImpl().onDrawBehind(canvas);
+        super.draw(canvas);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        getImpl().onDrawOver(canvas);
+        super.onDraw(canvas);
+    }
+
+    private ButtonCompatImpl getImpl() {
+        if (mImpl == null) {
+            mImpl = createImpl();
+        }
+        return mImpl;
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        getImpl().onSizeChanged();
+    }
+
+    @Override
+    protected boolean verifyDrawable(@NonNull Drawable who) {
+        return super.verifyDrawable(who) || getImpl().verifyDrawable(who);
+    }
+
+    @Override
+    public void jumpDrawablesToCurrentState() {
+        super.jumpDrawablesToCurrentState();
+        getImpl().jumpDrawablesToCurrentState();
+    }
+
+    @Override
+    protected void drawableStateChanged() {
+        super.drawableStateChanged();
+        getImpl().drawableStateChanged();
+    }
+
+
+    private ButtonCompatImpl createImpl() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return new ButtonCompatLollipop(this);
+        } else {
+            return new ButtonCompatGingerbread(this);
+        }
+    }
 }
